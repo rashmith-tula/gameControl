cdba4c8f811f588d1b00647627d31d7c85118136
Rashmith <rashmith.tula@gmail.com>
2019-02-21 16:40:08 +0530

    first commit of my game

---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+  @Output() intervalStarted = new EventEmitter<number>();
+  interval;
+  lastNumber = 0;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onStart() {
+    this.interval = setInterval(() => { 
+     this.intervalStarted.emit(this.lastNumber + 1);
+     this.lastNumber++;
+    }, 1000)
+  }
+
+  onStop() {
+    clearInterval(this.interval);
+  }
+
+}
